ffdcae2d80ba848542eb77dc50f19485f7562c4c
Unhidra <unhidra@example.com>
2025-11-19 20:33:31 -0500

    autocommit 20251119-203331

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,7 +1,8 @@
 use reqwest::Client;
 use serde::{Serialize, Deserialize};
 use tokio_tungstenite::connect_async;
-use tungstenite::Message;
+use tokio_tungstenite::tungstenite::Message;
+use futures_util::{SinkExt, StreamExt};
 
 #[derive(Serialize)]
 struct LoginReq {
@@ -16,39 +17,56 @@ struct LoginResp {
 
 #[tokio::main]
 async fn main() {
+    println!("--- UNHIDRA CLIENT ---");
+
     let http = Client::new();
 
-    println!("Logging in to Auth API...");
-    let resp = http.post("http://127.0.0.1:9200/login")
+    println!("Logging into Auth API...");
+    let res = http.post("http://127.0.0.1:9200/login")
         .json(&LoginReq {
             username: "user".into(),
             password: "password".into(),
         })
         .send()
         .await
-        .unwrap()
-        .json::<LoginResp>()
+        .expect("Failed to send login request");
+
+    let parsed = res.json::<LoginResp>()
         .await
-        .unwrap();
+        .expect("Failed to parse login response");
 
-    println!("TOKEN = {}", resp.token);
+    println!("Login success. Token = {}", parsed.token);
 
-    let ws_url = format!("ws://127.0.0.1:9000/ws?token={}", resp.token);
-    println!("Connecting to {}", ws_url);
+    let ws_url = format!("ws://127.0.0.1:9000/ws?token={}", parsed.token);
+    println!("Connecting to WebSocket: {}", ws_url);
 
-    let (ws_stream, _) = connect_async(ws_url).await.unwrap();
+    let (ws_stream, _) = connect_async(ws_url)
+        .await
+        .expect("Failed to connect to WebSocket");
+
+    println!("Connected to Gateway WebSocket.");
+
     let (mut write, mut read) = ws_stream.split();
 
     tokio::spawn(async move {
-        while let Some(msg) = read.next().await {
-            println!("WS RECEIVED: {:?}", msg);
+        loop {
+            tokio::time::sleep(std::time::Duration::from_secs(3)).await;
+            let _ = write.send(Message::Text("ping from client".into())).await;
         }
     });
 
-    write.send(Message::Text("{\"msg\":\"hello world\"}".into()))
-        .await
-        .unwrap();
+    while let Some(msg) = read.next().await {
+        match msg {
+            Ok(Message::Text(text)) => {
+                println!("Received: {}", text);
+            }
+            Ok(_) => {}
+            Err(e) => {
+                println!("WebSocket error: {}", e);
+                break;
+            }
+        }
+    }
 
-    println!("Client finished sending message.");
-    loop { tokio::time::sleep(std::time::Duration::from_secs(1)).await; }
+    println!("WebSocket closed.");
 }
